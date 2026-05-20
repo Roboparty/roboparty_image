55106a7cbdd5aa4d479a17aca250b66d37ca771c
wentywenty <2321901849@qq.com>
2026-05-20 12:49:21 +0800

    add series overlay dts and enable new canfd-sdmmc and fix chrome deb install and build robopi3

--- scripts/image-helpers.sh
+++ scripts/image-helpers.sh
@@ -268,7 +268,7 @@ dpkg_install_debs_chroot()
 	for package in "${deb_packages[@]}"; do
 		package_names+=($(dpkg-deb -f "$package" Package))
 
-		dep_str=$(dpkg-deb -I "${package}" | grep 'Depends' | sed 's/.*: //' | sed 's/ //g' | sed 's/([^)]*)//g')
+		dep_str=$(dpkg-deb -f "${package}" Depends | tr -d '\n' | sed 's/ //g' | sed 's/([^)]*)//g')
 		IFS=',' read -ra dep_array <<< "$dep_str"
 
 		if [[ ! ${#dep_array[@]} -eq 0 ]]; then
@@ -276,12 +276,10 @@ dpkg_install_debs_chroot()
 
 			for element in "${dep_array[@]}"; do
 				if [[ $element == *"|"* ]]; then
-					#dep_array=("${dep_array[@]/$element}")
-					:
-				else
-					if ! find_in_array "$element" "${package_dependencies[@]}"; then
-						package_dependencies+=("${element}")
-					fi
+					element="${element%%|*}"
+				fi
+				if ! find_in_array "$element" "${package_dependencies[@]}"; then
+					package_dependencies+=("${element}")
 				fi
 			done
 
